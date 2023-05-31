80a1d1f02f7591b80feb209ac88219e66101fb0d
Andrei Gubani <andrei.gubani22@gmail.com>
2023-05-31 11:47:42 +0200

    Added Class to store Variables

---
+++ src/MyVars.java
@@ -0,0 +1,30 @@
+public class MyVars {
+    private int intRightWords;
+    private int intWrongWords;
+    private boolean isVoiceToggleSelected;
+
+    public void setIntRightWords(int intRightWords) {
+        this.intRightWords = intRightWords;
+    }
+
+    public void setIntWrongWords(int intWrongWords) {
+        this.intWrongWords = intWrongWords;
+    }
+
+    public void setVoiceToggleSelected(boolean isVoiceToggleSelected) {
+        this.isVoiceToggleSelected = isVoiceToggleSelected;
+    }
+
+    public int getIntRightWords() {
+        return intRightWords;
+    }
+
+    public int getIntWrongWords() {
+        return intWrongWords;
+    }
+
+    public boolean isVoiceToggleSelected() {
+        return isVoiceToggleSelected;
+    }
+
+}
