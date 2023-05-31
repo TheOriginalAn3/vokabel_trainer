92924fa781c4251f15fa834e436dd8f36a7e52d4
Andrei Gubani <andrei.gubani22@gmail.com>
2023-05-31 18:41:48 +0200

    Prepared code for Selin 🙃

--- EngPythonRecognizer.py
+++ EngPythonRecognizer.py
@@ -1,23 +1,24 @@
 import speech_recognition as sr
 import sys
 
+
+# ******************* DEIN CODE COMMT HIER *******************
+
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
     try:
         # recognize speech using Google Speech Recognition
-        # transcription = r.recognize_google(audio)
-        
-        # transcription = r.recognize_google(audio, language='de-DE') #For german recognition
         transcription = r.recognize_google(audio, language='en-US') # For American English recognition
         
-        sys.stdout.write(transcription)
-        sys.stdout.flush
+        # ******************* DEIN CODE KOMMT HIER *******************
     except sr.UnknownValueError:
-        sys.stdout.write("Google Speech Recognition could not understand audio")
-        sys.stdout.flush
+        print("Google Speech Recognition could not understand audio")
+        # ******************* DEIN CODE KOMMT HIER *******************
     except sr.RequestError as e:
-        sys.stdout.write("Could not request results from Google Speech Recognition service")
-        sys.stdout.flush
-sys.exit(0)
+        print("Could not request results from Google Speech Recognition service")
+        # ******************* DEIN CODE KOMMT HIER *******************
+        
+sys.exit(0) # Programm schliesst von selber. Mit java neustarten falls nötig
+# ******************* HIER NICHTS SCHREIBEN *******************
--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Somit kann man nur 1 einziges objekt/instanz davon erstellen
+        SpeechRecognition sr = SpeechRecognition.getInstance();
+
+        //English -> Deutsch kommentar unten entfernen
+        //sr.startEngRecognizer();
+
+        //Deutsch -> English kommentar unten entfernen
+        //sr.starGerRecognizer();
     }
 }
