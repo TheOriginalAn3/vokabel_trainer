70343ce038db1ef98bc580e6eb39db4e7c2f015e
Andrei Gubani <andrei.gubani22@gmail.com>
2023-05-31 12:04:07 +0200

    Made MyVars a Singleton class

--- src/MyVars.java
+++ src/MyVars.java
@@ -3,6 +3,23 @@ public class MyVars {
     private int intWrongWords;
     private boolean isVoiceToggleSelected;
 
+    private static MyVars instance = null;
+
+    // Trick I saw on TikTok (Singleton method to only allow one object of this class to exist)
+    private MyVars() {
+        // Exists only to defeat instantiation.
+    }
+    public static MyVars getInstance() {
+        if(instance == null) {
+            instance = new MyVars();
+        } else {
+            System.out.println("MyVars Object already exists!!!");
+        }
+        return instance;
+    }
+    // This marks the end of the trick i saw on TikTok
+
+
     public void setIntRightWords(int intRightWords) {
         this.intRightWords = intRightWords;
     }
