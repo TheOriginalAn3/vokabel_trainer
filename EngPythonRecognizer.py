33b7deb14989ab0febbb5db53d966eaf591ba6f0
Selin <selinulku05@gmail.com>
2023-06-19 12:43:24 +0200

    Letzter Stand SOCKET

--- EngPythonRecognizer.py
+++ EngPythonRecognizer.py
@@ -3,10 +3,17 @@ import sys
 import socket
 
 # ******************* DEIN CODE COMMT HIER *******************
+#platzhalter ändern
+
 soc = socket.sock()
 host = "localhost"
 port = 1303
 soc.bind((host, port))
+print("Hingewiesener Port:", port)
+c, addr = soc.accept()
+
+String = "Hallo, Andrei"
+binaer = String.encode()
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -16,13 +23,15 @@ with sr.Microphone() as source:
         # recognize speech using Google Speech Recognition
         transcription = r.recognize_google(audio, language='en-US') # For American English recognition
         print(transcription)
-        # ******************* DEIN CODE KOMMT HIER *******************
+        c.send(String)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
-        # ******************* DEIN CODE KOMMT HIER *******************
+        
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service")
         # ******************* DEIN CODE KOMMT HIER *******************
         
+    
+    socket.close()  
 sys.exit(0) # Programm schliesst von selber. Mit java neustarten falls nötig
 # ******************* HIER NICHTS SCHREIBEN *******************
--- src/SpeechRecognition.java
+++ src/SpeechRecognition.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class SpeechRecognition {
     private static SpeechRecognition instance = null;
@@ -8,13 +10,14 @@ public class SpeechRecognition {
     private Process p;
     private String transcription;
 
-    // Trick I saw on TikTok (Singleton method to only allow one object of this class to exist)
+    // Trick I saw on TikTok (Singleton method to only allow one object of this
+    // class to exist)
     private SpeechRecognition() {
         // Normal constructor
     }
 
     public static SpeechRecognition getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             System.out.println("SpeechRecognition Object not existent...");
             instance = new SpeechRecognition();
         } else {
@@ -27,15 +30,34 @@ public class SpeechRecognition {
 
     // TODO #19 Create a programm to start the Python Script
     public void startEngRecognizer() {
-            try {
-                pb = new ProcessBuilder("python", "EngPythonRecognizer.py");
-                p = pb.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-                p.destroy();
-            }
-            //transcription = getTranscriptionFromPython();
+        // System.out.println("An3");
+        try {
+            pb = new ProcessBuilder("python", "EngPythonRecognizer.py");
+            p = pb.start();
+            System.out.println("An2");
+            Socket socket = new Socket();
+            int port = 1303;
+
+            System.out.println("Hingewiesener Port:" + port);
+
+            InputStream inputStream = socket.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = inputStream.read(buffer);
+
+            String receivedString = new String(buffer, 0, bytesRead);
+            System.out.println("Von PYthon geschickt:" + receivedString);
+
+            socket.close();
+        } catch (IOException e) {
+            // System.out.println("An1");
+            e.printStackTrace();
+            p.destroy();
+        } catch (Exception a) {
+            // System.out.println("An0");
+            a.printStackTrace();
         }
+        // transcription = getTranscriptionFromPython();
+    }
 
     public void startGerRecognizer() {
         try {
@@ -45,9 +67,6 @@ public class SpeechRecognition {
             e.printStackTrace();
             p.destroy();
         }
-        //transcription = getTranscriptionFromPython();
+        // transcription = getTranscriptionFromPython();
     }
-
-    // TODO Create a method to get the transcription from Python
-    
 }
